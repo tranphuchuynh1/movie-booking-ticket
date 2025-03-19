b496a2183d9d8b8c670dc229a1b76a488d1bdcb5
anhnhantothi <tothianhnhan17@gmail.com>
2025-03-19 09:27:39 +0700

    feat_N : Add colors.dart file

---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color tdBlack = Color(0x00000000);
+const Color tdBlackLight = Color(0x000b0b0b);
+const Color tdWhite = Color(0xffffffff);
+const Color tdRed = Color.fromARGB(255, 203, 66, 16);
+const Color tdYellow = Color(0x00e1cd17);
+const Color tdGrey = Color(0x00051615);
